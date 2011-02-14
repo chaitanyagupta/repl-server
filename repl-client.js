b3522c1add8a2f2d518cf4ca9c952ff3d9be0f6a
Chaitanya Gupta <chaitanya.g@directi.com>
2011-02-14 18:50:00 +0530

    Basic system of sid/rid works. Inspired by BOSH.

--- repl-client.js
+++ repl-client.js
@@ -1,35 +1,65 @@
 var ReplClient = function (url) {
+    var self = this;
     var abort = false;
+    var getHttpRequest, log;
     var isTitanium = (typeof Titanium !== "undefined");
-    var log;
 
     if (isTitanium) {
         log = function (msg) {
             Ti.API.log('log', msg);
         };
+        getHttpRequest = function () {
+            return Titanium.Network.createHTTPClient();
+        };
     } else {
         log = function (msg) {
             console.log(msg);
         };
+        getHttpRequest = function () {
+            return new XMLHttpRequest();
+        };
     }
 
-    var httpRequest = function (result) {
-        var req;
-        if (isTitanium) {
-            req = Titanium.Network.createHTTPClient();
-        } else {
-            req = new XMLHttpRequest();
-        }
-        req.open('GET', url + '?result=' + encodeURIComponent(result), true);
+    var start = function () {
+        var req = getHttpRequest();
+        req.open('POST', url + '/start', true);
         req.onreadystatechange = function (evt) {
             if (req.readyState === 4) {
                 if (req.status === 200) {
-                    log("To evaluate: " + req.responseText);
-                    evalRequest(req.responseText);
+                    log("response text is", req.responseText);
+                    var response = JSON.parse(req.responseText);
+                    self.sid = response.sid;
+                    self.rid = Math.floor(Math.random() * 10000000);
+                    sendResult(null);
                 }
             }
         };
-        req.send(null);
+        req.send();
+    };
+
+    var stop = function () {
+        abort = true;
+        var req = getHttpRequest();
+        req.open('POST', url + '/' + self.sid + '/stop', true);
+        req.send();
+    };
+
+    var sendResult = function (result) {
+        var req = getHttpRequest();
+        req.open('POST', url + '/' + self.sid + '/eval', true);
+        req.onreadystatechange = function (evt) {
+            if (req.readyState === 4) {
+                if (req.status === 200) {
+                    var response = JSON.parse(req.responseText);
+                    log('To evaluate: ' + response.query);
+                    evalRequest(response.query);
+                }
+            }
+        };
+        req.send(JSON.stringify({
+            rid: self.rid++,
+            result: result
+        }));
     };
 
     var evalRequest = function (string) {
@@ -46,7 +76,7 @@ var ReplClient = function (url) {
             props.thrown = true;
         }
         if (!abort) {
-            httpRequest(JSON.stringify(props));
+            sendResult(JSON.stringify(props));
         }
     };
 
@@ -92,9 +122,7 @@ var ReplClient = function (url) {
     };
 
 
-    this.stop = function () {
-        abort = true;
-    };
+    self.stop = stop;
 
-    httpRequest('null');
+    start();
 };
--- repl-server.lisp
+++ repl-server.lisp
@@ -1,39 +1,102 @@
 (cl:defpackage #:repl-server
   (:use #:cl #:hunchentoot)
   (:export #:start-server
-           #:stop-server))
+           #:stop-server)
+  (:shadow #:session #:*session*))
 
 (cl:in-package #:repl-server)
 
-(defvar *eval-condition-var* (bt:make-condition-variable))
-(defvar *eval-lock* (bt:make-lock))
-(defvar *eval-string* nil)
-(defvar *result-string* nil)
+(defvar *sessions* (make-hash-table))
+(defvar *sessions-lock* (bt:make-lock))
 
-(defun eval-string (string)
-  (bt:with-lock-held (*eval-lock*)
-    (setf *eval-string* string)
-    (bt:condition-notify *eval-condition-var*))
-  (bt:with-lock-held (*eval-lock*)
-    (loop
-       (bt:condition-wait *eval-condition-var* *eval-lock*)
-       (when *result-string*
-         (return (prog1 (json:decode-json-from-string *result-string*)
-                   (setf *eval-string* nil
-                         *result-string* nil)))))))
+(defvar *session*)
 
-(define-easy-handler (eval-string-handler :uri "/eval-string")
-    ((result :parameter-type 'string))
-  (bt:with-lock-held (*eval-lock*)
-    (setf *result-string* result)
-    (bt:condition-notify *eval-condition-var*))
-  (bt:with-lock-held (*eval-lock*)
+(defstruct (session (:conc-name %session-))
+  (sid (random 1000000000) :read-only t)
+  (rid)
+  (lock (bt:make-lock) :read-only t)
+  (condition-var (bt:make-condition-variable) :read-only t)
+  (eval-src nil)
+  (result nil))
+
+(defmacro def-session-property (name accessor &key read-only)
+  `(progn
+     (defun ,name (&optional (session *session*))
+       (,accessor session))
+     ,(unless read-only
+       `(defun (setf ,name) (value &optional (session *session*))
+          (setf (,accessor session) value)))))
+
+(def-session-property sid %session-sid :read-only t)
+(def-session-property rid %session-rid)
+(def-session-property session-lock %session-lock :read-only t)
+(def-session-property session-condition-var %session-condition-var :read-only t)
+(def-session-property eval-src %session-eval-src)
+(def-session-property result %session-result)
+
+(defun new-session ()
+  (let ((session (make-session)))
+    (bt:with-lock-held (*sessions-lock*)
+      (setf (gethash (session-sid session) *sessions*) session))))
+
+(defun get-session ()
+  (let* ((sid (parse-integer (second (cl-ppcre:split "/" (request-uri*)))))
+         (session (bt:with-lock-held (*sessions-lock*)
+                    (gethash sid *sessions*))))
+    (unless session
+      (setf (return-code*) +http-not-found+)
+      (abort-request-handler))
+    session))
+
+(define-easy-handler (start-handler :uri "/start")
+    ()
+  (let ((session (new-session)))
+    (setf (content-type*) "application/json")
+    (json:encode-json-to-string `((:sid . ,(session-sid session))))))
+
+(define-easy-handler (stop-handler :uri (lambda (request) (cl-ppcre:scan "^/.*/stop$" (request-uri* request))))
+    ()
+  (let ((session (get-session)))
+    (bt:with-lock-held (*sessions-lock*)
+      (remhash (session-sid session) *sessions*))
+    nil))
+
+(define-easy-handler (eval-handler :uri (lambda (request) (cl-ppcre:scan "^/.*/eval$" (request-uri* request))))
+    ()
+  (let* ((*session* (get-session))
+         (response-json (raw-post-data :force-text t))
+         (response (json:decode-json-from-string response-json))
+         (rid (dot response :rid))
+         (result (dot response :result)))
+    (setf (rid) rid)
+    (when result
+      (bt:with-lock-held ((session-lock))
+        (setf (result) result)
+        (bt:condition-notify (session-condition-var))))
+    (bt:with-lock-held ((session-lock))
+      (loop
+         (bt:condition-wait (session-condition-var) (session-lock))
+         (when (eval-src)
+           (setf (content-type*) "application/json")
+           (return (json:encode-json-to-string (list (cons :query (eval-src))))))))))
+
+(defmethod handle-request :around ((acceptor acceptor) (request request))
+  (setf (header-out :access-control-allow-origin) "*")
+  (setf (header-out :access-control-allow-headers) (header-in* :access-control-request-headers))
+  (unless (eql (request-method*) :options)
+    (call-next-method)))
+
+(defun eval-string (string *session*)
+  (bt:with-lock-held ((session-lock))
+    (setf (eval-src) string)
+    (bt:condition-notify (session-condition-var)))
+  (bt:with-lock-held ((session-lock))
     (loop
-       (bt:condition-wait *eval-condition-var* *eval-lock*)
-       (setf (header-out :access-control-allow-origin) "*")
-       (setf (content-type*) "text/javascript")
-       (when *eval-string*
-         (return *eval-string*)))))
+       (bt:condition-wait (session-condition-var) (session-lock))
+       (when (result)
+         (return (prog1 (result)
+                   (setf (eval-src) nil
+                         (result) nil)))))))
 
 (defun eval-loop ()
   (loop
