75955fff9395dc55a78deb47a73459f5f559870e
Chaitanya Gupta <chaitanya.g@directi.com>
2011-02-25 00:24:46 +0530

    Add colour customization in example-launch.lisp

--- example-launch.lisp
+++ example-launch.lisp
@@ -1,5 +1,11 @@
 (ql:quickload :repl-server)
 
+(repl-server:defcolor :repl :style :bright)
+(repl-server:defcolor :result :fg :reset)
+(repl-server:defcolor :info :fg :reset)
+(repl-server:defcolor :warn :fg :yellow)
+(repl-server:defcolor :error :fg :red)
+
 (repl-server:start-server)
 
 (repl-server:start-repl :color-output t :exit-on-finish t)
