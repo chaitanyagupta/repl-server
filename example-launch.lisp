fd8bb0a90ec405deb0c307ab2fa926f16f1721fb
Chaitanya Gupta <chaitanya.g@directi.com>
2011-03-21 00:55:58 +0530

    Change default colors in example-launch.lisp.

--- example-launch.lisp
+++ example-launch.lisp
@@ -1,8 +1,8 @@
 (ql:quickload :repl-server)
 
 (repl-server:defcolor :repl :style :bright)
-(repl-server:defcolor :result :fg :reset)
-(repl-server:defcolor :info :fg :reset)
+(repl-server:defcolor :result :fg :white)
+(repl-server:defcolor :info :style :dim :fg :reset)
 (repl-server:defcolor :warn :fg :yellow)
 (repl-server:defcolor :error :fg :red)
 
